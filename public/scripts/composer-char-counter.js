7d30a2decb473376626155e4bbed0f8325e035ba
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-24 16:04:13 -0700

    added tweet counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    let textLength = $(this).val().length;
+    let remainingChars = 140 - textLength;
+    $(".counter").text(remainingChars);
+
+    if (remainingChars < 0) {
+      $(".counter").addClass("invalid");
+    } else {
+      $(".counter").removeClass("invalid");
+    }
+  });
+});
